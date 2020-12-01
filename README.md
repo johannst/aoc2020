8f35183f3d504697113497f965085748c68e0ce8
johannst <johannes.stoelp@gmail.com>
2020-12-01 19:33:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## [Advent of Code 2020](https://adventofcode.com/2020) [![Build Status](https://travis-ci.org/johannst/aoc2020.svg?branch=master)](https://travis-ci.org/johannst/aoc2020)
+
+```
+ ____ ____ ____ _________ ____ ____ ____ ____
+||A |||o |||C |||       |||2 |||0 |||2 |||0 ||
+||__|||__|||__|||_______|||__|||__|||__|||__||
+|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|
+```
+
+For fun and profit!
+
+```zsh
+# Project overview and instructions
+cargo run
+
+# Maximize profit
+cargo run | lolcat -a
+```
+
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("--- Happy Advent of Code 2020 ---");
+    println!("");
+    println!("This project is organized as follows:");
+    println!("  src/bin/dayN ............ solution of day N");
+    println!("  input/dayN .............. input for day N");
+    println!("");
+    println!("Actions:");
+    println!("  cargo build --bins ...... build all binaries at once");
+    println!("  cargo run --bin dayN .... run binary for day N ");
+    println!("  cargo test --bins ....... run all tests in all binaries");
+}
