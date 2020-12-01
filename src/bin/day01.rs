47ba63813d8172e73346e42be5d9fd0aea78801e
johannst <johannes.stoelp@gmail.com>
2020-12-01 20:45:02 +0100

    add test to day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,30 +1,57 @@
 use aoc20::read_input;
 
-fn main() {
-    let input = read_input("day01");
-
-    let input_num: Vec<_> = input
+fn parse_input() -> Vec<usize> {
+    read_input("day01")
         .split_whitespace()
         .map(|input| input.parse::<usize>().expect("Input is not a number!"))
-        .collect();
+        .collect()
+}
 
-    'outer: for (i, n1) in input_num.iter().enumerate() {
-        for n2 in input_num.iter().skip(i) {
+fn challenge1(input: &Vec<usize>) -> Option<usize> {
+    for (i, n1) in input.iter().enumerate() {
+        for n2 in input.iter().skip(i) {
             if n1 + n2 == 2020 {
-                println!("Magic Numbers ({} * {}) = {}", n1, n2, n1 * n2);
-                break 'outer;
+                println!("Magic Number ({} * {}) = {}", n1, n2, n1 * n2);
+                return Some(n1 * n2);
             }
         }
     }
+    None
+}
 
-    'outer2: for (i, n1) in input_num.iter().enumerate() {
-        for (j, n2) in input_num.iter().skip(i).enumerate() {
-            for n3 in input_num.iter().skip(j) {
+fn challenge2(input: &Vec<usize>) -> Option<usize> {
+    for (i, n1) in input.iter().enumerate() {
+        for (j, n2) in input.iter().skip(i).enumerate() {
+            for n3 in input.iter().skip(j) {
                 if n1 + n2 + n3 == 2020 {
                     println!("Magic Number ({} * {} * {}) = {}", n1, n2, n3, n1 * n2 * n3);
-                    break 'outer2;
+                    return Some(n1 * n2 * n3);
                 }
             }
         }
+    }
+    None
+}
+
+fn main() {
+    let input = parse_input();
+    challenge1(&input);
+    challenge2(&input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_challange1() {
+        let result = challenge1(&parse_input());
+        assert_eq!(result, Some(876459));
+    }
+
+    #[test]
+    fn check_challange2() {
+        let result = challenge2(&parse_input());
+        assert_eq!(result, Some(116168640));
     }
 }
